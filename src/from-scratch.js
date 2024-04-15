14be27d09c240f607f8160a8283fff06707cc53f
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-15 14:21:02 -0400

    Answered Q1

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,5 +1,13 @@
-const myForEach = () => {
+const myForEach = (arr, callback) => {
+  for (let i = 0; i < arr.length; i++) {
+    callback(arr[i])
+  }
 };
+const nums = [1, 2, 3];
+const doubledNums = [];
+const addToDoubledNums = (num) => doubledNums.push(num * 2);
+
+console.log(myForEach(nums, addToDoubledNums))
 
 const myMap = () => {
 };
