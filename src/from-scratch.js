7c1eb9c86270d6909f496b07a7967eb2ad810623
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-16 10:56:49 -0400

    Answered Q2

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -3,14 +3,21 @@ const myForEach = (arr, callback) => {
     callback(arr[i])
   }
 };
-const nums = [1, 2, 3];
-const doubledNums = [];
-const addToDoubledNums = (num) => doubledNums.push(num * 2);
+// const nums = [1, 2, 3];
+// const doubledNums = [];
+// const addToDoubledNums = (num) => doubledNums.push(num * 2);
 
-console.log(myForEach(nums, addToDoubledNums))
+// console.log(myForEach(nums, addToDoubledNums))
 
-const myMap = () => {
+const myMap = (arr, callback) => {
+  const double = []
+  for (let i = 0; i < arr.length; i++) {
+    double.push(callback(arr[i]))
+  }
+  return double
+
 };
+
 
 const myFind = () => {
 };
