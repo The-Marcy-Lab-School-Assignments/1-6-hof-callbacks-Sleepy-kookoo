20ee2fba44a5c05fd4a940580fcd39bd3954fe8e
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-04-17 10:31:14 -0400

    Answered Q9

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -86,7 +86,17 @@ const sortUsersByOrder = (arrOfObj) => {
   return sorted
 };
 
-const sortUsersByName = () => {
+const sortUsersByName = (arrOfName) => {
+  const sorted = [...arrOfName]
+  sorted.sort((a, b) => {
+    if (a.name > b.name) {
+      return 1;
+    } else if (a.name < b.name) {
+      return -1;
+    }
+    return 0;
+  });
+  return sorted
 };
 
 module.exports = {
